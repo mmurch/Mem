922f8903a450167eefc6152961388a4611f141c6
mmurch <murchigras@gmail.com>
2015-04-18 14:55:00 -0400

    switch to hogan-express

--- backbone/app.js
+++ backbone/app.js
@@ -6,13 +6,13 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hjs');
+app.set('view engine', 'html');
+app.engine('html', require('hogan-express'));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -24,7 +24,6 @@ app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -60,7 +59,7 @@ app.use(function(err, req, res, next) {
 
 var server = app.listen(3000, function () {
     console.log('Memory game app started');
-});;
+});
 
 
 module.exports = app;
