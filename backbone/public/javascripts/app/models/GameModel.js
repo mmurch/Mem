9721819bd0316258e56d0a22a4b2e1876ed456b0
mmurch <murchigras@gmail.com>
2015-04-19 15:57:16 -0400

    added selection logic

--- backbone/public/javascripts/app/models/GameModel.js
+++ backbone/public/javascripts/app/models/GameModel.js
@@ -14,12 +14,49 @@ var Mem = Mem || {};
             this.setCards(new mem.CardCollection(null, {
                 game: this
             }));
+
+            this.getCards().on('change:isTesting', this.handleTestingChange, this);
         },
 
         maySelect: function(){
+            return this.getTestingCards().length < 2;
+        },
+
+        getTestingCards: function(){
             return this.getCards().filter(function(card){
                 return card.getIsTesting();
-            }).length < 2;
+            });
+        },
+
+        handleTestingChange: function(){
+            var testingCards = this.getTestingCards();
+            if (testingCards.length < 2){
+                return;
+            }
+
+            if (testingCards.length === 2){
+                if (testingCards[0].getSharedId() === testingCards[1].getSharedId()) {
+                    _.each(testingCards, function(card){
+                        card.setIsFound(true);
+                        card.setIsTesting(false, { silent: true });
+                    }, this);
+                }
+                else {
+                    this.setIsLocked(true);
+                    var _this = this;
+                    setTimeout(function(){
+                        _this.unlockGame();
+                    }, 800);
+                }
+            }
+        },
+
+        unlockGame: function(){
+            var testingCards = this.getTestingCards();
+            _.each(testingCards, function(card){
+                card.setIsTesting(false);
+            }, this);
+            this.setIsLocked(false);
         }
     });
 
